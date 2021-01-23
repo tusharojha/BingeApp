f149702a3746f8962a7cdb9edc0930e1798e43f3
Tushar Ojha <tusharojha2001@gmail.com>
2021-01-23 19:49:26 +0530

    updated config & flutter_svg

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,4 +2,4 @@ const TOKEN_ROUTE = 'lskflskjflksdjds';
 const JSON_HEADER = <String, String>{
   'Content-Type': 'application/json; charset=UTF-8',
 };
-const BASE_URL = 'https://53117f70b796.ngrok.io';
+const BASE_URL = 'https://bingeserver.herokuapp.com/';
